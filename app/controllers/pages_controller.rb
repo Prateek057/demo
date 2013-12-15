c701cb30febf6bc848d2bffc08c8a4c142d14b51
Prateek057 <bagrecha.prateek@gmail.com>
2013-12-15 06:54:33 -0600

    Edit Gemfile devise

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def mwhome
+  end
+
+  def about
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Demo::Application.routes.draw do
+
+  get 'about' => 'pages#about'
+  root :to => 'pages#mwhome'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
