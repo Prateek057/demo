ca71b7fc8be8ccc722aea58fd8775fdce7c69e94
Prateek057 <bagrecha.prateek@gmail.com>
2013-12-16 16:03:31 -0600

    Add Users Show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Demo::Application.routes.draw do
 
+  get "users/show"
+
   resources :pins
 
 
   devise_for :users
+
+   match 'users/:id' => 'users#show', as: :user
 
   get 'about' => 'pages#about'
   root :to => 'pins#index'
